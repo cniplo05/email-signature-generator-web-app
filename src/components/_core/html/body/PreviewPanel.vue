57fe3794411b1f54d2d3291cdec4294e56db38d1
juan.carlos <juan.carlos@rakwireless.com>
2020-10-13 22:33:09 +0800

    add: tab and tabs template

---
+++ src/components/_core/html/body/PreviewPanel.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col bg-dark-dark">
+                <span>To: <strong>Your Recipient</strong> </span>
+                <span>Subject: <strong>Check out my new Email Signature</strong></span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h1>Juan Dela Cruz</h1>
+                <span>09955554535</span>
+                <span>juan.carlos@rakwireless.com</span>
+                <span>Philippines</span>
+            </div>
+        </div>
+    </div>
+</template>
